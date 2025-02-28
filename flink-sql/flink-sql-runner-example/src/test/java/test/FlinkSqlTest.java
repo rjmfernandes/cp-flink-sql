4bcd8b65f4f39d93cc37a7da46b5770ae7773d8f
rjmfernandes <rjmfernandes@gmail.com>
2025-02-28 16:20:38 +0100

    updated to handle insert in test

--- flink-sql/flink-sql-runner-example/src/test/java/test/FlinkSqlTest.java
+++ flink-sql/flink-sql-runner-example/src/test/java/test/FlinkSqlTest.java
@@ -1,13 +1,17 @@
 package test;
 
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.*;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FlinkSqlTest {
     @Test
     void testValuesConnector() {
-        TableEnvironment tableEnv = TableEnvironment.create(EnvironmentSettings.inBatchMode());
+        StreamExecutionEnvironment execEnv = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        TableEnvironment tableEnv = StreamTableEnvironment.create(execEnv);
 
         // Register 'VALUES' connector
         tableEnv.executeSql(
@@ -17,8 +21,12 @@ public class FlinkSqlTest {
                         ") WITH ('connector' = 'values')"
         );
 
+        tableEnv.executeSql("INSERT INTO test_table VALUES (1, 'Alice'), (2, 'Bob')");
+        Table result = tableEnv.sqlQuery("SELECT COUNT(*) FROM test_table");
+
+
         // Verify the table exists
         Assertions.assertEquals("test_table", tableEnv.listTables()[0]);
+        //Assertions.assertEquals(2,result.execute().collect().next().getField(0));
     }
 }
-
