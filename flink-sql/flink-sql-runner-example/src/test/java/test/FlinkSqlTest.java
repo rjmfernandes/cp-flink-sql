e4170c25c32a961d33a9ceaa686f621b0a5d8bec
rjmfernandes <rjmfernandes@gmail.com>
2025-02-28 13:36:56 +0100

    unit test

---
+++ flink-sql/flink-sql-runner-example/src/test/java/test/FlinkSqlTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import org.apache.flink.table.api.*;
+import org.junit.jupiter.api.Test;
+
+public class FlinkSqlTest {
+    @Test
+    void testValuesConnector() {
+        TableEnvironment tableEnv = TableEnvironment.create(EnvironmentSettings.inBatchMode());
+
+        // Register 'VALUES' connector
+        tableEnv.executeSql(
+                "CREATE TABLE test_table (" +
+                        "  id INT, " +
+                        "  name STRING " +
+                        ") WITH ('connector' = 'values')"
+        );
+
+        // Verify the table exists
+        System.out.println(tableEnv.listTables());  // Should print "test_table"
+    }
+}
+
